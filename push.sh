78590f781e62c3dbcbfebf624a3d508ac63bfdf2
Vo Minh Thu <thu@hypered.io>
2016-05-15 17:25:09 +0200

    Add some note/hint/reminder.

--- push.sh
+++ push.sh
@@ -5,3 +5,10 @@ mv SHA1.tmp SHA1
 git add SHA1
 git commit -m"New SHA1: $(cat SHA1 | cut -f 1 -d ' ')."
 git push
+
+echo '-----------------------------------------------------------'
+echo 'After a minute or two, you can run'
+echo '  > docker pull images.reesd.com/reesd/hello'
+echo '  > docker run images.reesd.com/reesd/hello cat /BUILD-INFO'
+echo '  > git rev-parse HEAD'
+echo 'Both commit hash should match.'
